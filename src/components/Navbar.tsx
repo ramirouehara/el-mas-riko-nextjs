a6c599372090a29271e57ec32ef1caf5a48bbd54
Ramiro Uehara <uehararamiro@gmail.com>
2026-08-02 17:20:52 -0300

    hicimos el nav

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import Link from "next/link";
+
+
+export default function Navbar() {
+    return(
+        <nav className="flex items-center justify-between px-4 py-3">
+            <Link href="/">
+                <Image src="/img/logo.png" alt="El Mas Riko" width={140} height={60} />
+            </Link>
+
+            <ul className="flex items-center gap-3">
+                <li>
+                    <Link href="/">Inicio</Link>
+                </li>
+                <li>
+                    <Link href="/#nosotros">Nosotros</Link>
+                </li>
+                <li>
+                    <Link href="/#especialidades">Productos</Link>
+                </li>
+                <li>
+                    <Link href="/carta">Carta</Link>
+                </li>
+                <li>
+                    <Link href="/pedidos">Pedido</Link>
+                </li>
+                <li>
+                    <Link href="/login">Empleados</Link>
+                </li>
+            </ul>
+
+        </nav>
+    );
+}
