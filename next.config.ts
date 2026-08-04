1e8a8655bf34e4b133866942f57ca484986231e9
Ramiro Uehara <info.ramirouehara@gmail.com>
2026-08-04 12:22:15 -0300

    lista la fase 3

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,26 @@ const nextConfig: NextConfig = {
         protocol:"https",
         hostname:"statics.diariomendoza.com.ar",
         pathname:"/**",
+      },
+      {
+        protocol:"https",
+        hostname:"upload.wikimedia.org",
+        pathname:"/**",
+      },
+      {
+        protocol:"https",
+        hostname:"images.getrecipekit.com",
+        pathname:"/**",
+      },
+      {
+        protocol:"https",
+        hostname:"i0.wp.com",
+        pathname:"/**",
+      },
+      {
+        protocol:"https",
+        hostname:"www.semanarioextra.com.ar",
+        pathname:"/**",
       }
     ],
   }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,37 @@
 import Image from "next/image";
 import HeroCarousel from "@/components/HeroCarousel";
 
+
+const especialidades = [
+  {
+    src:"https://www.clarin.com/img/2021/09/06/ZUZbnAZNY_1256x620__2.jpg",
+    nombre:"Sanguches"
+  },
+  {
+    src:"https://upload.wikimedia.org/wikipedia/commons/9/96/Milanesa_napolitana_con_papas_fritas_rodajas_de_tomate_y_or%C3%A9gano.jpg",
+    nombre:"Minutas"
+  },
+  {
+    src:"https://images.getrecipekit.com/20220816143651-1.png?class=16x9",
+    nombre:"Empanadas"
+  },
+  {
+    src:"https://i0.wp.com/cremigal.com/wp-content/uploads/2022/04/CREMIGAL-10-scaled.jpg?w=1920&ssl=1",
+    nombre:"Pizzas"
+  },
+  {
+    src:"https://www.semanarioextra.com.ar/wp-content/uploads/2022/01/56afe82bc46188be6a8b4606.jpg",
+    nombre:"Bebidas"
+  }
+]
+
+
 export default function Home() {
   return (
     <>
+
       <HeroCarousel />
+
       <section id="nosotros" className="py-16">
         <div className="max-w-6xl mx-auto px-4">
           <div className="flex flex-col md:flex-row items-center gap-8">
@@ -32,6 +59,32 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+
+
+
+
+      <section id="especialidades" className="py-16">
+        <div className="max-w-6xl mx-auto px-4">
+          <h2 className="text-3xl font-bold text-center mb-8">NUESTRAS ESPECIALIDADES ❤️❤️❤️</h2>
+
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+            {especialidades.map((e) => {
+              return (
+              <div key={e.nombre}>
+                <div className="relative h-[300px]">
+                  <Image src={e.src} alt={e.nombre} fill className="object-cover"/>
+                </div>
+                  <h3 className="text-center mt-2 font-semibold">{e.nombre}</h3>
+              </div>
+              )
+            })}
+
+          </div>
+        </div>
+      </section>
+
+
     </>
   );
 }
